74547ab4750104c21714b68259ec5d4a6565576c
bogdan <bs2u13@soton.ac.uk>
2017-05-28 15:11:00 +0100

    Initial Commit

---
+++ Data-Types/Python_List.py
@@ -0,0 +1,16 @@
+#Consider a list (list = []). You can perform the following commands:
+#
+#    insert i e: Insert integer  at position .
+#    print: Print the list.
+#    remove e: Delete the first occurrence of integer .
+#    append e: Insert integer  at the end of the list.
+#    sort: Sort the list.
+#    pop: Pop the last element from the list.
+#    reverse: Reverse the list.
+
+
+if __name__ == '__main__':
+        N = int(input())
+        for i in range(0,N):
+            command = input()
+
---
+++ Intro/Python_readInput.py
@@ -0,0 +1,9 @@
+def read():
+    s = input() 
+    return s
+
+
+
+if __name__ == '__main__':
+        my_str = read()
+        print(my_str)
